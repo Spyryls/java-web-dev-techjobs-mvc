5cb13b320f4d3d4d1f06b80b92e70085d367f9bb
Spyryls <yr410ad@gmail.com>
2019-12-26 08:46:59 -0600

    Completed; working on in-search search bonus mission

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -30,7 +30,7 @@ public class SearchController {
     @PostMapping(value = "results")
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
         ArrayList<Job> jobs;
-        if (searchType.toLowerCase().equals("all") || searchType.equals("")){
+        if (searchType.toLowerCase().equals("all") && searchType.equals("")){
             jobs = JobData.findAll();
             model.addAttribute("title", "All Jobs");
         } else {
